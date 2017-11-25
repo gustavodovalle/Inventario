49e4ec460e74ebe856dd1871512d023001446bdc
gustavodovalle <32673209+gustavodovalle@users.noreply.github.com>
2017-11-25 15:09:32 -0200

    Add files via upload

---
+++ PROJETO/Login.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+content-type 
+read $name
+read $senha
+
+.PerfilAdm{
+	if [[ $name == admin ]] && [[ $senha == admin@132 ]];
+	then
+	<meta http-equiv="refresh" content="3; PerfilAdm.html">
+	fi
+}
+
